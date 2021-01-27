f9db8790647eb57ce17b15de409988e6ad1df0c6
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-27 12:56:45 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ function regSW(){
   }
 }
 
+// check to see if notifications are supported by this browser
+if (!('Notification' in window)) {
+  console.log('This browser does not support notifications!');
+  return;
+}
+
